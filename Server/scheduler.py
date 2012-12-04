b6b3bb1ad46f49e9af60ef754b735f798c27dead
Owen <owenchu@gmail.com>
2012-12-03 20:23:31 -0800

    Send email notification when compute job is done.

--- Server/scheduler.py
+++ Server/scheduler.py
@@ -3,9 +3,12 @@
 import os
 import pyinotify
 import shutil
+import smtplib
 import stat
+import string
 import subprocess
 import sys
+import yaml
 
 #
 # Limitations:
@@ -13,35 +16,19 @@ import sys
 #     are copied to the job queue folder.
 #
 
-VISTRAILS_SCRIPT_PATH = '/home/owenchu/architecture/vistrails-src-2.0.1-5e35e2b83b90/scripts/run_vistrails_batch_xvfb.sh'
-VISTRAILS_OUTPUT_PATH = '/home/owenchu/public_html/vistrails-output'
-VISTRAILS_WEB_OUTPUT_PATH = 'http://ok.freya.cc/~owenchu/vistrails-output'
-
-#WORKFLOW = {
-#    'offscreen.vt': 'offscreen',
-#    'brain_vistrail.vt': 'brain',
-#    'head.vt': 'aliases',
-#    'triangle_area.vt': 'Surface Area with Map',
-#    'spx.vt': 'contour',
-#    'protein_visualization.vt': 'Protein Visualization'
-#}
+SETTING = yaml.load(open('setting.yml', 'r'))
 
 class Scheduler:
-    def __init__(self, queue_path, config_path, running_path,
-                 done_path, result_path):
-        self.queue_path = queue_path
-        self.handler = self.Handler(queue_path=queue_path,
-                                    config_path=config_path,
-                                    running_path=running_path,
-                                    done_path=done_path,
-                                    result_path=result_path)
+    def __init__(self, paths):
+        self.paths = paths
+        self.handler = self.Handler(paths=paths)
 
     def run(self):
         wm = pyinotify.WatchManager()
         notifier = pyinotify.Notifier(wm, self.handler)
-        wm.add_watch([self.queue_path, VISTRAILS_OUTPUT_PATH],
+        wm.add_watch([self.paths['queue'], SETTING['vistrails']['output_path']],
                      pyinotify.IN_CLOSE_WRITE|pyinotify.IN_CREATE)
-        self.log('Start monitoring %s' % queue_path)
+        self.log('Start monitoring %s' % self.paths['queue'])
         notifier.loop()
 
     @staticmethod
@@ -49,52 +36,59 @@ class Scheduler:
         print("[Scheduler] " + msg)
 
     class Handler(pyinotify.ProcessEvent):
-        def my_init(self, queue_path, config_path, running_path,
-                    done_path, result_path):
-            self.queue_path = queue_path
-            self.config_path = config_path
-            self.running_path = running_path
-            self.done_path = done_path
-            self.result_path = result_path
+        def my_init(self, paths):
+            self.paths = paths
+            self.notified = {}
 
         def execute_job(self, job_path):
             Scheduler.log("About to execute job: " + job_path)
 
             _, filename = os.path.split(job_path)
-            running_filepath = os.path.join(self.running_path, filename)
+            self.running_project = self.project_name(filename)
 
+            running_filepath = os.path.join(self.paths['running'], filename)
+
+            # Remove old project file with the same name
             try:
                 Scheduler.log('Removing ' + running_filepath)
                 os.unlink(running_filepath)
             except OSError:
                 pass
 
-            Scheduler.log('Moving %s to %s' %(job_path, self.running_path))
+            # Move the job to 'running' folder
+            Scheduler.log('Moving %s to %s' %(job_path, self.paths['running']))
             os.rename(job_path, running_filepath)
 
-            self.result_filepath = os.path.join(self.result_path,
-                                                self.project_name(filename) + '.txt')
+            self.result_filepath = os.path.join(self.paths['result'],
+                                        self.project_name(filename) + '.txt')
             Scheduler.log('Project: ' + self.result_filepath)
 
             workflow = self.workflow_for(filename)
             Scheduler.log('Workflow: ' + workflow)
 
-            cmd_args = [VISTRAILS_SCRIPT_PATH, running_filepath, workflow,
-                        VISTRAILS_OUTPUT_PATH]
+            # Run VisTrails
+            cmd_args = [SETTING['vistrails']['script_path'],
+                        running_filepath, workflow,
+                        SETTING['vistrails']['output_path']]
             Scheduler.log(' '.join(cmd_args))
             subprocess.call(cmd_args)
             Scheduler.log('Done')
 
-            done_filepath = os.path.join(self.done_path, filename)
-            Scheduler.log('Moving %s to %s' %(running_filepath, self.done_path))
+            # Move the job to 'done' folder
+            done_filepath = os.path.join(self.paths['done'], filename)
+            Scheduler.log('Moving %s to %s' % (running_filepath,
+                                               self.paths['done']))
             os.rename(running_filepath, done_filepath)
 
+            # Trigger notification
+            self.notified[self.running_project] = False
+
         def process_IN_CLOSE(self, event):
-            if event.pathname.startswith(self.queue_path):
+            if event.pathname.startswith(self.paths['queue']):
                 self.execute_job(event.pathname)
 
         def process_IN_CREATE(self, event):
-            if event.pathname.startswith(VISTRAILS_OUTPUT_PATH):
+            if event.pathname.startswith(SETTING['vistrails']['output_path']):
                 Scheduler.log('chmod 644 ' + event.pathname)
                 os.chmod(event.pathname,
                          stat.S_IRUSR|stat.S_IWUSR|stat.S_IRGRP|stat.S_IROTH)
@@ -102,7 +96,13 @@ class Scheduler:
                     f = open(self.result_filepath, "a")
                     try:
                         _, filename = os.path.split(event.pathname)
-                        f.write(VISTRAILS_WEB_OUTPUT_PATH + '/' + filename + '\n')
+                        url = SETTING['vistrails']['web_output_path'] + '/' + filename
+                        f.write(url + '\n')
+
+                        if not self.notified[self.running_project]:
+                            Scheduler.log('Sending notification for ' + self.running_project)
+                            self.send_notification(self.running_project, url)
+                            self.notified[self.running_project] = True
                     finally:
                         f.close()
                 except IOError:
@@ -110,7 +110,7 @@ class Scheduler:
 
         def workflow_for(self, vistrails_project_filename):
             filename, extention = os.path.splitext(vistrails_project_filename)
-            config_filepath = os.path.join(self.config_path, filename + ".cfg")
+            config_filepath = os.path.join(self.paths['config'], filename + ".cfg")
             workflow = ''
 
             try:
@@ -129,7 +129,29 @@ class Scheduler:
         def project_name(self, vistrails_project_filename):
             return os.path.splitext(vistrails_project_filename)[0]
 
-if __name__ == '__main__':
+        def send_notification(self, vistrails_project_name, url):
+            to = 'owen.chu@sv.cmu.edu'
+            body = string.join((
+                    'From: %s' % SETTING['notification']['sender'],
+                    'To: %s' % to,
+                    'Subject: [HECC] Your compute job was completed',
+                    '',
+                    'Your compute job "%s" was completed.' % vistrails_project_name,
+                    '',
+                    'You can launch VisTrails or go the the following page to check the results:',
+                    url
+                    ), "\r\n")
+
+            server = smtplib.SMTP(SETTING['notification']['smtp_server'],
+                                  SETTING['notification']['smtp_port'])
+            server.starttls()
+            server.login(SETTING['notification']['sender'],
+                         SETTING['notification']['sender_password'])
+            server.sendmail(SETTING['notification']['sender'], [to], body)
+            server.quit()
+
+
+def main():
     if len(sys.argv) < 6:
         print >> sys.stderr, "Command line error: missing argument(s)."
         sys.exit(1)
@@ -138,12 +160,17 @@ if __name__ == '__main__':
         return path if os.path.isabs(path) else os.path.join(os.getcwd(), path)
 
     # Required arguments
-    queue_path = compose_path(sys.argv[1])
-    config_path = compose_path(sys.argv[2])
-    running_path = compose_path(sys.argv[3])
-    done_path = compose_path(sys.argv[4])
-    result_path = compose_path(sys.argv[5])
+    paths = {
+        'queue': compose_path(sys.argv[1]),
+        'config': compose_path(sys.argv[2]),
+        'running': compose_path(sys.argv[3]),
+        'done': compose_path(sys.argv[4]),
+        'result': compose_path(sys.argv[5])
+    }
 
     # Blocks monitoring
-    Scheduler(queue_path, config_path, running_path,
-              done_path, result_path).run()
+    Scheduler(paths).run()
+
+
+if __name__ == '__main__':
+    main()
